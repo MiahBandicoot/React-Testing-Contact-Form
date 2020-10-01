f74046c9f8812f2ec2c7d9210a5aa9984db59ac8
Miah <revolution5117@gmail.com>
2020-10-01 12:13:46 -0700

    Project Complete

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,30 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
+import ContactForm from './components/ContactForm'
 
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("", () => {
+
+})
+
+// test("renders First Name without crashing", () => {
+//   render(<ContactForm/>);
+//   const firstNameInput = screen.getLabelText(/firstName!/i);
+  
+
+//   fireEvent.change(firstNameInput, {target:{name:'firstName', value:'Ed'}});
+
+//   const newName = screen.findByText{/Mia!/i}
+//   expect(firstNameInput).toBeInTheDocument();
+// })
+
+test("", () => {
+
+})
+test("", () => {
+
+})
